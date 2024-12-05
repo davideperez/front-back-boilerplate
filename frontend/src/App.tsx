ea562fed5c26781d282fd512a3563214f32f9863
David <david.ezequiel.perez@gmail.com>
2024-12-05 18:39:56 -0300

    frontend: Tailwind and ShadCN added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './index.css';
+import { Button } from './components/ui/button';
 
 const App: React.FC = () => {
-  return <h1>Hello, React + Vite + TypeScript!</h1>;
-};
+  return (
+  <div className="m-6 font-mono">
+    <h1 className="text-3xl font-bold underline">Hello, React + Vite + TypeScript!</h1>
+    <Button>Presione aquí</Button>
+  </div>
+)};
 
 export default App;
