f2a6426a2ed8c198582e0ee6916a8157e204fd5e
David <david.ezequiel.perez@gmail.com>
2024-12-05 20:05:29 -0300

    backend: helmet added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Uses
+
+app.use(helmet());
 
 // Morgan según el entorno sea desarrollo o producción
 if (process.env.NODE_ENV === 'production') {
@@ -13,6 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   app.use(morgan('dev'));  // Para desarrollo, más compacto y con colores
 }
+
+// Rutas
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo desde Node.js con TypeScript y Express!!!');
